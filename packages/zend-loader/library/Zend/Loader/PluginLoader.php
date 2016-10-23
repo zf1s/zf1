1fa357baffb0c2f2644ac50fb07486877da8044c
Maciej Holyszko <mh@ovos.at>
2016-10-23 15:58:35 +0200

    Release 1.12.20

--- packages/zend-loader/library/Zend/Loader/PluginLoader.php
+++ packages/zend-loader/library/Zend/Loader/PluginLoader.php
@@ -389,7 +389,7 @@ class Zend_Loader_PluginLoader implements Zend_Loader_PluginLoader_Interface
         foreach ($registry as $prefix => $paths) {
             $className = $prefix . $name;
 
-            if (class_exists($className, false)) {
+            if (class_exists($className)) {
                 $found = true;
                 break;
             }
