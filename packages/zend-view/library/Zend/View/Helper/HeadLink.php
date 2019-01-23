e3bf641c9170145db2ec7252c625c27566533b81
mg <mg@ovos.at>
2019-01-23 18:16:56 +0100

    fix for missing combine() variable $extras, causing notice on php 7.3 error 'Notice' with message 'compact(): Undefined variable: extras'

--- packages/zend-view/library/Zend/View/Helper/HeadLink.php
+++ packages/zend-view/library/Zend/View/Helper/HeadLink.php
@@ -374,6 +374,7 @@ class Zend_View_Helper_HeadLink extends Zend_View_Helper_Placeholder_Container_S
         $media                 = 'screen';
         $conditionalStylesheet = false;
         $href                  = array_shift($args);
+        $extras                = []; // php 7.3 fix
 
         if ($this->_isDuplicateStylesheet($href)) {
             return false;
@@ -440,6 +441,7 @@ class Zend_View_Helper_HeadLink extends Zend_View_Helper_Placeholder_Container_S
         $href  = array_shift($args);
         $type  = array_shift($args);
         $title = array_shift($args);
+        $extras = []; // php 7.3 fix
 
         if(0 < count($args) && is_array($args[0])) {
             $extras = array_shift($args);
