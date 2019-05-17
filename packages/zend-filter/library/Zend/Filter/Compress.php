6c9a05fe7144ba61bd3b2619a8050672d4dea136
Maciej Holyszko <14310995+falkenhawk@users.noreply.github.com>
2019-05-17 20:58:11 +0200

    [zend-filter] fix auto-loading compress adapters

--- packages/zend-filter/library/Zend/Filter/Compress.php
+++ packages/zend-filter/library/Zend/Filter/Compress.php
@@ -96,7 +96,9 @@ class Zend_Filter_Compress implements Zend_Filter_Interface
 
         $adapter = $this->_adapter;
         $options = $this->getAdapterOptions();
-        if (!class_exists($adapter)) {
+        if (class_exists('Zend_Filter_Compress_' . ucfirst($adapter))) {
+            $adapter = 'Zend_Filter_Compress_' . ucfirst($adapter);
+        } elseif (!class_exists($adapter)) {
             // require_once 'Zend/Loader.php';
             if (Zend_Loader::isReadable('Zend/Filter/Compress/' . ucfirst($adapter) . '.php')) {
                 $adapter = 'Zend_Filter_Compress_' . ucfirst($adapter);
