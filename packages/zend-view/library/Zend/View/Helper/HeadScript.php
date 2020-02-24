256e43f868990cb71c99dcdf64ee6ba8944f8413
Piotr Niziniecki <pniziniecki@gmail.com>
2020-02-24 23:46:06 +0100

    The type attribute is unnecessary for JavaScript resources.
    Signed-off-by: Elan Ruusamäe <glen@pld-linux.org>

--- packages/zend-view/library/Zend/View/Helper/HeadScript.php
+++ packages/zend-view/library/Zend/View/Helper/HeadScript.php
@@ -439,7 +439,12 @@ class Zend_View_Helper_HeadScript extends Zend_View_Helper_Placeholder_Container
         $addScriptEscape = !(isset($item->attributes['noescape']) && filter_var($item->attributes['noescape'], FILTER_VALIDATE_BOOLEAN));
 
         $type = ($this->_autoEscape) ? $this->_escape($item->type) : $item->type;
-        $html  = '<script type="' . $type . '"' . $attrString . '>';
+        if ($this->view->doctype()->isHtml5() && $type === 'text/javascript') {
+            $html  = '<script' . $attrString . '>';
+        } else {
+            $html  = '<script type="' . $type . '"' . $attrString . '>';
+        }
+
         if (!empty($item->source)) {
             $html .= PHP_EOL ;
 
