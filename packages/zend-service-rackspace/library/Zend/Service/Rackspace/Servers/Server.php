6740163728b1010c1200848e10f2b780558dcf02
Maciej Holyszko <14310995+falkenhawk@users.noreply.github.com>
2020-05-22 18:05:46 +0200

    [zend-service-rackspace] restore back `array_key_exists` in place of `isset`
    reverted unnecessary changes from https://github.com/zf1s/zf1/pull/16/files#diff-7d8cdc4dbd5afcd88fca225eaf9a353f

--- packages/zend-service-rackspace/library/Zend/Service/Rackspace/Servers/Server.php
+++ packages/zend-service-rackspace/library/Zend/Service/Rackspace/Servers/Server.php
@@ -104,11 +104,11 @@ class Zend_Service_Rackspace_Servers_Server
             // require_once 'Zend/Service/Rackspace/Servers/Exception.php';
             throw new Zend_Service_Rackspace_Servers_Exception(self::ERROR_PARAM_CONSTRUCT);
         }
-        if (!isset($data['name'])) {
+        if (!array_key_exists('name', $data)) {
             // require_once 'Zend/Service/Rackspace/Servers/Exception.php';
             throw new Zend_Service_Rackspace_Servers_Exception(self::ERROR_PARAM_NO_NAME);
         }
-        if (!isset($data['id'])) {
+        if (!array_key_exists('id', $data)) {
             // require_once 'Zend/Service/Rackspace/Servers/Exception.php';
             throw new Zend_Service_Rackspace_Servers_Exception(self::ERROR_PARAM_NO_ID);
         }
