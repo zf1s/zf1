3443f4836ac5e05b4970a85ef103b5efc7e7ad76
Elan Ruusamäe <glen@pld-linux.org>
2021-03-15 17:41:58 +0200

    Use close() method instead of calling fclose() directly

--- packages/zend-mail/library/Zend/Mail/Storage/Mbox.php
+++ packages/zend-mail/library/Zend/Mail/Storage/Mbox.php
@@ -297,7 +297,7 @@ class Zend_Mail_Storage_Mbox extends Zend_Mail_Storage_Abstract
         $this->_filemtime = filemtime($this->_filename);
 
         if (!$this->_isMboxFile($this->_fh, false)) {
-            @fclose($this->_fh);
+            $this->close();
             /**
              * @see Zend_Mail_Storage_Exception
              */
